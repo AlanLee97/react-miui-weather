a615d09fd118dfa58cfdce04ffe98f4bceba1f4e
AlanLee <1445654576@qq.com>
2023-09-03 02:33:03 +0800

    fix: 调整组件导入

--- src/components/index.js
+++ src/components/index.js
@@ -1,11 +1,7 @@
-import HeaderCpn from './header/Header';
-import MainInfoCpn from './main-info/MainInfo';
-import ThumbDaysInfoCpn from './thumb-days-info/ThumbDaysInfo';
-import Forecast24HoursCpn from './forecast-24hours/Forecast24Hours';
-import OtherInfoCpn from './other-info/OtherInfo';
-
-export const Header = HeaderCpn;
-export const MainInfo = MainInfoCpn;
-export const ThumbDaysInfo = ThumbDaysInfoCpn;
-export const Forecast24Hours = Forecast24HoursCpn;
-export const OtherInfo = OtherInfoCpn;
+export { default as Header } from './header/Header';
+export { default as MainInfo } from './main-info/MainInfo';
+export { default as ThumbDaysInfo } from './thumb-days-info/ThumbDaysInfo';
+export { default as Forecast24Hours } from './forecast-24hours/Forecast24Hours';
+export { default as OtherInfo } from './other-info/OtherInfo';
+export { default as ForecastVideo } from './forecast-video/ForecastVideo';
+export { default as IconWrapper } from './common/icon-wrapper/IconWrapper';
