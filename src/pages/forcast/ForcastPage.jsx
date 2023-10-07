8597cdc2bec39765b8e2199f46b33a7971a7413c
AlanLee <1445654576@qq.com>
2023-10-07 22:55:17 +0800

    feat: 增加降水预报页面内容

--- src/pages/forcast/ForcastPage.jsx
+++ src/pages/forcast/ForcastPage.jsx
@@ -1,12 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './style.scss';
+
+function CustomTitle(props = {}) {
+  const [data, setData] = useState({
+    area: '宝安区',
+    address: '银田支一路'
+  });
+  return (
+    <section className="cpn--custom-title">
+      <div className='title-text'>降水预报</div>
+      <div className='address-info'>
+        <span className='area-text'>{data.area}</span>
+        <span className='address-text'>{data.address}</span>
+      </div>
+    </section>
+  );
+}
+
+function ForcastMap(props = {}) {
+  return (
+    <section className="cpn--forcast-map">
+      {/* 地图显示不是本项目重点，暂时使用图片代替，后续有兴趣再开发 */}
+      <img src={require('../../assets/imgs/forcast-map.jpg')} className='img-forcast-map' />
+    </section>
+  );
+}
 
 export default function ForcastPage(props = {}) {
   const { BasePage } = window.$components;
+
   return (
-    <BasePage name='forcast' title='降雨预报'>
-      <section className="page--forcast">
-        降雨预报
-      </section>
+    <BasePage name='forcast'>
+      <CustomTitle />
+      <ForcastMap />
     </BasePage>
   );
 }
